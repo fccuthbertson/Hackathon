fda5f5bdcd70d57d9414da9cdb863c4fb56baf08
Frank Cuthbertson <fccuthbertson@gmail.com>
2018-04-22 20:43:29 -0700

    Working simple logit reg model

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -8,7 +8,7 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('GUL001_Appt_Data_fixed.csv')
 
-# Splits Appt Date and Start tIime into 2 separate columns. 
+# Splits Appt Date and Start time into 2 separate columns. 
 data2 = dataset['ApptStartTime'].apply(lambda x: pd.Series(x.split(' ')))
 data2.rename(columns={0:'ApptDate',1:'StartTime'},inplace=True)
 
@@ -16,23 +16,41 @@ data2.rename(columns={0:'ApptDate',1:'StartTime'},inplace=True)
 data3 = dataset['ApptEndTime'].apply(lambda x: pd.Series(x.split(' ')))
 data3.rename(columns={0:'ApptEndDate', 1:'EndTime'}, inplace=True)
 
+#drop unecessary columns
+ds = dataset.drop(['Id','ApptStartTime','ApptEndTime','ApptCreatedDate','ApptStatus','ApptType','ApptLocation','ApptProviderName','PatientId','AccountNumber','PatientZip'], axis=1)
+# drop unnamed column and dependent var
+cols_of_interest = [1,2,3,4,5,6,7,8,9,10]
+X = ds.iloc[:,cols_of_interest].values
 
-print(dataset.describe())
+#set dependent var for appt Status
+y = ds.iloc[:, 1].values
+# set appt status to either 0(no show) or 1(completed) for simplicity
+for i in range(len(y)):
+    if y[i] == 6 or y[i] == 10:
+        y[i] = 1
+    else:
+        y[i] = 0    
 
-X = dataset.iloc[:, :-1].values
+#take care of categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelEncoder_X = LabelEncoder()
+X[:, 6] = labelEncoder_X.fit_transform(X[:, 6]) # Gender
+X[:, 7] = labelEncoder_X.fit_transform(X[:, 7]) # Marital Status
+X[:, 8] = labelEncoder_X.fit_transform(X[:, 8]) # Language
+X[:, 9] = labelEncoder_X.fit_transform(X[:, 9]) # Ethnicity
+oneHotEncoder = OneHotEncoder(categorical_features = [6,7,8,9])
+X = oneHotEncoder.fit_transform(X).toarray()
+y = labelEncoder_X.fit_transform(y)
 
-#taking care of missing data
-from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
-imputer.fit
+
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+y_train = sc_y.fit_transform(y_train)
---
+++ simple_logit_regression.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 22 18:29:16 2018
+
+@author: A 
+"""
+    
+
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+import matplotlib.pyplot as plt 
+plt.rc("font", size=14)
+from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import train_test_split
+import seaborn as sns
+sns.set(style="white")
+sns.set(style="whitegrid", color_codes=True)
+
+data = pd.read_excel('GUL001_Appt_Data_fixed.xlsx').drop(['Unnamed: 22'], axis=1)
+data = data[data['Age'] > 61]
+data = data[data['Age'] < 109]
+data['Age'].value_counts()
+data['ApptStatusId'] = data['ApptStatusId'].map(lambda x: 1 if x in [6,15,3,4,17,14] else 0)
+data = data.drop(['Id','ApptStartTime','ApptEndTime','ApptCreatedDate','ApptStatus','ApptTypeId','ApptFacilityId','ApptLocation','ApptProviderName','ApptProviderId','PatientId','AccountNumber','PatientZip'], axis=1)
+
+
+"""%matplotlib inline
+pd.crosstab(data.MaritalStatus,data.ApptStatusId).plot(kind='bar')
+plt.title('Makes Appt for Marital Status')
+plt.xlabel('MaritalStatus')
+plt.ylabel('ApptStatusId')
+plt.savefig('purchase_fre_job')"""
+
+# get dummy vars 
+cat_vars= ['ApptType','Gender','MaritalStatus','Language','Ethnicity']
+for var in cat_vars:
+    cat_list='var'+'_'+var
+    cat_list = pd.get_dummies(data[var], prefix=var)
+    data1=data.join(cat_list)
+    data=data1
+    
+cat_vars= ['ApptType','Gender','MaritalStatus','Language','Ethnicity']
+data_vars=data.columns.values.tolist()
+to_keep=[i for i in data_vars if i not in cat_vars]
+
+data_final=data[to_keep]
+data_final.columns.values
+
+data_final_vars=data_final.columns.values.tolist()
+y=['ApptStatusId']
+X=[i for i in data_final_vars if i not in y]
+
+
+from sklearn import datasets
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
+logreg = LogisticRegression()
+rfe = RFE(logreg, 18)
+rfe = rfe.fit(data_final[X], data_final[np.asarray(y)] )
+print(rfe.support_)
+print(rfe.ranking_)
+
+
+cols=['Age','TotalPatientCancelled','ApptType_1ST POST OP','ApptType_6 MONTHS','ApptType_B SCAN',
+      'ApptType_CANCEL','ApptType_NEW PATIENT','ApptType_POST OP','ApptType_SAME DAY POST OP','Gender_F','Gender_M',
+      'ApptType_SURGERY SIGN UP','MaritalStatus_S','Language_eng','Language_por','Language_spa',
+      'Language_swa','Language_urd','Ethnicity_DE','Ethnicity_HS'] 
+X=data_final[cols]
+y=data_final['ApptStatusId']
+
+from scipy import stats
+stats.chisqprob = lambda chisq, df: stats.chi2.sf(chisq, df)
+import statsmodels.api as sm
+logit_model=sm.Logit(y,X)
+result=logit_model.fit()
+print(result.summary())
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+logreg = LogisticRegression()
+logreg.fit(X_train, y_train)
+
+y_pred = logreg.predict(X_test)
+print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(logreg.score(X_test, y_test)))
+
+from sklearn import model_selection
+from sklearn.model_selection import cross_val_score
+kfold = model_selection.KFold(n_splits=10, random_state=7)
+modelCV = LogisticRegression()
+scoring = 'accuracy'
+results = model_selection.cross_val_score(modelCV, X_train, y_train, cv=kfold, scoring=scoring)
+print("10-fold cross validation average accuracy: %.3f" % (results.mean()))
+
+from sklearn.metrics import confusion_matrix
+confusion_matrix = confusion_matrix(y_test, y_pred)
+print(confusion_matrix)
+
+from sklearn.metrics import classification_report
+print(classification_report(y_test, y_pred))
+
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
+logit_roc_auc = roc_auc_score(y_test, logreg.predict(X_test))
+fpr, tpr, thresholds = roc_curve(y_test, logreg.predict_proba(X_test)[:,1])
+plt.figure()
+plt.plot(fpr, tpr, label='Logistic Regression (area = %0.2f)' % logit_roc_auc)
+plt.plot([0, 1], [0, 1],'r--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic')
+plt.legend(loc="lower right")
+plt.savefig('Log_ROC')
+plt.show()
